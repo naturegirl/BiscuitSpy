26a2fca8bab85c2895c72471e1b2a7cf37096009
Marcela <msmelara@gmail.com>
2014-01-10 11:16:57 -0500

    preliminary profiler

--- src/com/BiscuitSpy/cookies/CookieBowl.java
+++ src/com/BiscuitSpy/cookies/CookieBowl.java
@@ -25,12 +25,13 @@ public class CookieBowl {
 	/** Creates a cookie bowl based on an HTTP packet passed in from the packet capture.
 	 * This assumes that the packet has been screened for an HTTP header and Cookie field.
 	 * 
-	 * @param packet a capture HTTP packet
+	 * @param packet a captured HTTP packet
 	 */
 	public CookieBowl(Http packet){
-		// TODO: extract source IP addr, dest IP addr and domain from packet
+		domain = packet.fieldValue(Request.Host);
 		bowl = fillCookieBowl(packet.fieldValue(Request.Cookie));
 		extracted = new Hashtable<String,Cookie>();
+		System.out.println("created cookie bowl for a request to domain: "+domain);
 	}
 
 	private Hashtable<String,Cookie> fillCookieBowl(String cookies){
@@ -41,17 +42,26 @@ public class CookieBowl {
 		Hashtable<String,Cookie> bowl = new Hashtable<String,Cookie>();		
 		ArrayList<String> list = CookieUtils.tokenize(cookies,";");
 		for(String cookieStr : list){
-			ArrayList<String> pair = CookieUtils.tokenize(cookieStr, "=");
-			// TODO extract cookie info from request, will have to pass in request or simply store entire packet associated with bowl
+			String[] cookieComps = new String[2];
+			cookieComps = cookieStr.split("=",2);
+			System.out.println("Created new cookie: name = "+cookieComps[0]+" value = "+cookieComps[1]);
+			Cookie cookie = new Cookie(cookieComps[0], cookieComps[1], domain);
+			bowl.put(cookieComps[0], cookie);
 		}
-		return null;
+		
+		return bowl;
 	}
 	
 	/** Using this bowl, separate out the cookies used for profiling
 	 * 
 	 */
 	public void extractProfileCookies(){
-		
+		for (String name : CookieUtils.spyCookies){
+			Cookie cookie = (Cookie)bowl.get(name);
+			if(cookie != null){
+				extracted.put(name, cookie);
+			}
+		}
 	}
 
 	
